618cd63c50ed8fa00bb1506ca7fa0344f16aabad
Murilo <murilorodriguesrrs@gmail.com>
2024-09-02 23:22:43 -0300

    refact

--- data/data.py
+++ data/data.py
@@ -1,12 +1,9 @@
 import kaggle
 import pandas as pd
 
-# Defina o caminho do arquivo onde o dataset será baixado
-dataset_name = 'username/dataset-name'  # substitua com o nome do dataset
-download_path = 'data/dataset.csv'  # substitua com o caminho desejado
+dataset_name = 'username/dataset-name'
+download_path = 'data/dataset.csv'
 
-# Baixe o dataset usando a API do Kaggle
 kaggle.api.dataset_download_file(dataset_name, file_name='filename.csv', path='data', unzip=True)
 
-# Carregue o dataset em um DataFrame
 df = pd.read_csv(download_path)
