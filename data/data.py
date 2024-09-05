d5dba0f1aed820e7c0be482fd73ada26e5ba1bd5
Murilo <murilorodriguesrrs@gmail.com>
2024-09-04 23:13:24 -0300

    chore: import libs

--- data/data.py
+++
@@ -1,9 +0,0 @@
-import kaggle
-import pandas as pd
-
-dataset_name = 'username/dataset-name'
-download_path = 'data/dataset.csv'
-
-kaggle.api.dataset_download_file(dataset_name, file_name='filename.csv', path='data', unzip=True)
-
-df = pd.read_csv(download_path)
