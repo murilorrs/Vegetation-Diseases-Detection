f40d97359a32e2c354c9b3602c2590b1e7697039
Murilo <murilorodriguesrrs@gmail.com>
2024-09-12 04:22:49 -0300

    update: separing unzip and finishing model

---
+++ src/unzip.py
@@ -0,0 +1,11 @@
+import os
+import zipfile
+
+zip_file_path = '../data/Plant-Dataset.zip'
+extract_dir = '../data'
+
+with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+    zip_ref.extractall(extract_dir)
+    
+if not os.path.exists(extract_dir):
+    os.makedirs(extract_dir)
